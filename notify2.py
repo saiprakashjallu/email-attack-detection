4f299df0c106654f9ac652057f7d652b776b8229
Sai Prakash <47019159+saiprakashjallu@users.noreply.github.com>
2025-07-09 08:49:52 -0400

    Update notify2.py

--- notify2.py
+++ notify2.py
@@ -4,12 +4,12 @@ from email.mime.text import MIMEText
 def test_notify():
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    sender_email = "mail.24gateway@gmail.com"
-    sender_password = "nriy eydm ugsq ccpb"
-    receiver_email = "saiprakashjallu@gmail.com"
+    sender_email = "Your Mail ID"
+    sender_password = "App Password"
+    receiver_email = "Receiver Mail ID"
 
-    subject = "🚨 TEST Notification"
-    body = "This is a test notification from your phishing email scanner."
+    subject = "TEST"
+    body = "This is a test notification from phishing email scanner."
 
     msg = MIMEText(body, "plain")
     msg["Subject"] = subject
@@ -21,7 +21,7 @@ def test_notify():
             server.starttls()
             server.login(sender_email, sender_password)
             server.send_message(msg)
-        print("✅ Test email sent.")
+        print("Test email sent.")
     except Exception as e:
         print(f"❌ Failed to send test email: {e}")
 
